69254e763917dee69c54df7a0f17dd1de7794f7b
Terra Huber <swiftruns26@gmail.com>
2019-07-25 15:51:32 -0500

    Add remove button to cart

--- js/cart.js
+++ js/cart.js
@@ -17,7 +17,7 @@ function renderCart() {
   showCart();
 }
 
-// TODO: Remove all of the rows (tr) in the cart table (tbody)
+//DONE: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
   var tbody = document.getElementsByTagName("tbody");
   tbody[0].innerHTML = '';
@@ -26,10 +26,20 @@ function clearCart() {
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
   // TODO: Find the table body
+  var tbody = document.getElementsByTagName("tbody");
   // TODO: Iterate over the items in the cart
+  for (var i = 0; i < Cart.length; i++){
   // TODO: Create a TR
+  var tr = document.createElement("tr");
   // TODO: Create a TD for the delete link, quantity,  and the item
+  var td = document.createElement("td");
+  var button = document.createElement("button");
+  button.textContent = " X ";
+  td.appendChild(button);
+  tr.appendChild(td);
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  }
+  
 }
 
 function removeItemFromCart(event) {
