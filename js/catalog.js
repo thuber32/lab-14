647d8fa2524419426b2c9c119fc236457380c1d5
Jacob Ervin <sonic4467@gmail.com>
2019-07-25 15:15:24 -0500

    got items working

--- js/catalog.js
+++ js/catalog.js
@@ -5,18 +5,22 @@
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
-  //TODO: Add an <option> tag inside the form's select for each product
+  //DONE: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById("items");
   for (var i in Product.allProducts) {
+    var opt = document.createElement('option');
+    opt.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(opt);
   }
 }
+populateForm();
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
-  // TODO: Prevent the page from reloading
-
+  // DONE: Prevent the page from reloading
+  event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
   saveCartToLocalStorage();
@@ -27,7 +31,9 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  
   // TODO: get the quantity
+
   // TODO: using those, create a new Cart item instance
 }
 
